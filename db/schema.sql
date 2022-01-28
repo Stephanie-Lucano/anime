d2c510d2833d48ab7351b4fcb612a34de6b46036
stephanie-lucano <stephaniemarielucano@pursuit.org>
2022-01-28 15:19:29 -0500

    Updated table name

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 -- DROP DATABASE IF EXISTS favanimes;
-CREATE DATABASE favanimes;
-\c favanimes;
-CREATE TABLE animes (
+CREATE DATABASE animes_dev;
+\c animes_dev;
+CREATE TABLE animes_dev (
     id SERIAL PRIMARY KEY,
     name TEXT,
     release INT
